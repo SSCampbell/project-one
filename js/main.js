fa51efa97fcb90d5edd4b51e207dec99b22041a9
unknown <Scampbell@spartaglobal.com>
2018-07-06 15:28:35 +0100

    PLAN: Writing a structured plan of how the project should run

---
+++ js/main.js
@@ -0,0 +1,16 @@
+//The user is presented with a game user manual
+//The user is presented with a prompt to take their name
+//The users name is presented on the game left-side
+//The game is presented in the center of the screen || The title is presented to the user
+//The battleships are loaded onto the grid field
+//The user can select which grid space they think the battleship will be
+//IF the user is successful it will produce an explosion in the water
+//Otherwise it will produce a sploosh animation
+//The user has a limited amount of cannons they can user
+//If the user successfully destroys each section of the ship the shipwreck will appear in the spaces it occupied
+//If the user successfully destroys all the ships on the field they are presented with a 
+//
+//
+//
+//
+//
