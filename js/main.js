7edb2ae12748cacb33c9926a8c15859e9ff1e5c1
unknown <Scampbell@spartaglobal.com>
2018-07-08 22:11:30 +0100

    BattleShip: Updating more battleships onto the grid

--- js/main.js
+++ js/main.js
@@ -31,27 +31,70 @@ var userProfile = function(){
 var cannonSize = 30;
 //ships = [miniSub,techSub, bigSub, sneakySub, dangerSub];
 var $grid = $("td");
-var i = null;
-var rand = Math.floor(Math.random() * 80);
-i = rand;
-var miniS = [];
+var rand = Math.floor(Math.random() * 81);
+var newRand;
+var i = rand;
+var miniSubArray = [];
+var techSubArray = [];
+var powerSubArray = [];
+var gridNumRandomized = [];
 var miniSNum = false;
+var techSNum = false;
+var powerSNum = false;
 //make a class for each sub
 // this would be used to target a cell and populate it with the class
 console.log(i);
+//miniSub
 $($grid[i]).addClass("miniSub").html("miniSubA");
-miniS[0] = i;
+miniSubArray[0] = i;
+gridNumRandomized[0] = i;
 miniSNum = true;
 if (miniSNum == true){
 $($grid[i + 1]).addClass("miniSub").html("miniSubB");
-miniS[1] = i + 1;
+miniSubArray[1] = i + 1;
+gridNumRandomized[1] = i;
 }
+
+numberRandomize();
+//techSub
+$($grid[i]).addClass("techSub").html("techSubA");
+techSubArray[0] = i;
+gridNumRandomized[2] = i;
+techSNum = true;
+if (techSNum == true){
+$($grid[i + 1]).addClass("techSub").html("techSubB");
+techSubArray[1] = i + 1;
+gridNumRandomized[3] = i;
+$($grid[i + 2]).addClass("techSub").html("techSubC");
+techSubArray[2] = i + 2;
+gridNumRandomized[3] = i;
+}
+
+numberRandomize();
+//powerSub
+$($grid[i]).addClass("powerSub").html("powerSubA");
+powerSubArray[0] = i;
+gridNumRandomized[4] = i;
+powerSNum = true;
+if (powerSNum == true){
+$($grid[i + 9]).addClass("powerSub").html("powersubB");
+powerSubArray[1] = i + 9;
+gridNumRandomized[5] = i;
+$($grid[i + 18]).addClass("powerSub").html("powersubC");
+powerSubArray[2] = i + 18;
+gridNumRandomized[6];
+$($grid[i + 27]).addClass("powerSub").html("powersubD");
+powerSubArray[3] = i + 27;
+gridNumRandomized[7];
+
+}
+
+
 
 $("td").click(function(){
 cannonSize--;
-if ($(this).html() == "miniSubA" || $(this).html() == "miniSubB")
+if ($(this).html() !== "")
 {
-  $(this).removeClass("miniSub");
 $(this).addClass("subHit");
 }
 else {
@@ -59,6 +102,14 @@ else {
 }
 
 });
+
+function numberRandomize(){
+  for (var i = 0; i < gridNumRandomized.length; i++) {
+    do{
+    rand = Math.floor(Math.random() * 81);
+    i = rand;
+    } while (rand === gridNumRandomized[i])}
+}
 
 
 if (cannonSize == 0)
